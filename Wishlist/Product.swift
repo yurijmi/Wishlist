095ff16a46d71a3142a51a0a7a3e0e369e577d02
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-07 03:51:56 +0300

    Created CoreData property Product

---
+++ Wishlist/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  Wishlist
+//
+//  Created by Юрий Михайлов on 07.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Product: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
