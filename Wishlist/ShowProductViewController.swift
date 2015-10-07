37e071570f505db55431b3b5a9e9765070fc0108
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-07 04:38:50 +0300

    Added ShowProduct view

---
+++ Wishlist/ShowProductViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShowProductViewController.swift
+//  Wishlist
+//
+//  Created by Юрий Михайлов on 07.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class ShowProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
